bcc4ab11eede5673837fa04363a5e4e96021c872
aongtosan <aongtosan@gmail.com>
2022-03-27 18:46:35 +0700

    refactor code && inti cursor move feature

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -6,11 +6,15 @@ public class TileController : MonoBehaviour
 {
     // Start is called before the first frame update
     bool isWalkAble;
-    bool isReachAble;
     bool isContainItem;
-   [SerializeField]
+    bool isCursorSelect;
+
+    [SerializeField]
     GameObject chestPrefab;
+
+    string tileId;
     int heightlvl;
+    const string NOT_REACHABLE = "Not Reachable";
     void Start()
     {
         if (isContainItem)
@@ -22,11 +26,15 @@ public class TileController : MonoBehaviour
         {
             tileColor.color = new Color32(253, 166, 76, 1);
         }
-        else if (isReachAble)
+        if (isWalkAble)
         {   
             tileColor.color = new Color32(41, 81, 243,1);
         }
-        else
+        if (isCursorSelect)
+        {
+            tileColor.color = Color.green;
+        }
+        else if(tileId.Contains(NOT_REACHABLE))
         {
             tileColor.color = Color.red;
         }
@@ -44,20 +52,25 @@ public class TileController : MonoBehaviour
         treasure.transform.localPosition = new Vector3(0, 0.5f, 0);
         Instantiate(chestPrefab, treasure.transform);
     }
+    public string TileId
+    {
+        get{ return tileId; }
+        set{tileId = value; }
+    }
     public bool IsWalkAble
     {
         get { return isWalkAble; }
         set { isWalkAble = value; }
     }
-    public bool IsReachAble
-    {
-        get { return isReachAble; }
-        set { isReachAble = value; }
-    }
     public bool IsContainItem
     {
         get { return isContainItem; }
         set { isContainItem = value; }
+    }
+    public bool IsCursorSelect
+    {
+        get { return isCursorSelect; }
+        set { isCursorSelect = value; }
     }
     public int Heightlvl
     {
--- Assets/Scripts/TileGenerate.cs
+++ Assets/Scripts/TileGenerate.cs
@@ -8,6 +8,9 @@ public class TileGenerate : MonoBehaviour
     [SerializeField]
     GameObject tilePrefabs;
     [SerializeField]
+    GameObject cursorPrefabs;
+
+    [SerializeField]
     int width;
     [SerializeField]
     int depth;
@@ -17,64 +20,85 @@ public class TileGenerate : MonoBehaviour
     int hillPercentage;
     [SerializeField]
     int treasurePile;
-  
+
+    const string NAME_PATTERN_TILE = "Tile[{0}][{1}][{2}]";
+    const string ID_PATTERN_TILE = "Tile[{0}][{1}]";
+    const string ROW_PATTERN_TILE = "Row[{0}]";
+    const string BASE_PLANE_NAME = "BASE";
+    const string NOT_REACHABLE = "Not Reachable";
+    const string CURSOR = "Cursor";
+
     private GameObject terrain;
-    private List<GameObject> tileList;
+    private Dictionary<string,GameObject> tileMap;
+    List<GameObject> tileList;
 
     private void Awake()
     {
-        terrain = new GameObject("Terrain");
+        terrain = new GameObject(BASE_PLANE_NAME);
         terrain.transform.SetParent(transform);
         terrain.transform.localPosition = Vector3.zero;
-        tileList = new List<GameObject>();
+        tileMap = new Dictionary<string, GameObject>();
+        
+        createHillTile(width, depth, height, hillPercentage, treasurePile);
+        Transform initCursorLoc = tileMap[string.Format(ID_PATTERN_TILE, 0, 0)].transform.parent;
+        TileController initCursorLocBlock = tileMap[string.Format(ID_PATTERN_TILE, 0, 0)].GetComponent<TileController>();
+
+        intiCursorPosition(initCursorLoc,initCursorLocBlock);
+        //intiCursorPosition(initCursorLoc);
+
+        //cursorMove();
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < depth; j++)
+            {
+                Debug.Log(tileMap[string.Format(ID_PATTERN_TILE, i, j)].GetComponent<TileController>().TileId);
+            }
+        }
     }
 
-    void Start()
+    void cursorMove(Transform changeTile, TileController currtileBlock, TileController nexttileBlock)
     {
-        createHillTile(width, depth, height,hillPercentage,treasurePile);
        
+    }
+    void intiCursorPosition(Transform position,TileController tileBlock)
+    {
+        GameObject cursor = new GameObject(CURSOR);
+        cursor.transform.SetParent(position);
+        cursor.transform.localPosition = new Vector3(0, 3f, 0);
+        cursor = Instantiate(cursorPrefabs, cursor.transform);
+        tileBlock.IsCursorSelect = true;
+        Debug.Log("selected tile => "+tileBlock.TileId);
     }
     void createHillTile(int widht,int depth,int limitHeight,int percentageHill,int treasureCount)
     {
-        for (int i = 0; i < depth; i++)
+        for (int i = 0; i < depth; i++) // row x
         {
-            GameObject row = new GameObject(string.Format("row[{0}]", i));
+            GameObject row = new GameObject(string.Format(ROW_PATTERN_TILE, i));
             row.transform.SetParent(terrain.transform);
             row.transform.localPosition = new Vector3(0, 0, i);
 
-            for (int j = 0; j < widht; j++)
+            for (int j = 0; j < widht; j++) // depth -> column z
             {
                int tileHeight = Random.Range(0, limitHeight);
                int ratio = Random.Range(0,101);
-               tileHeight = 100 - percentageHill < ratio ? tileHeight : 0;
-               int tresholdTreasureTile = widht * (depth / 2);
-                for (int w = 0; w <= tileHeight; w++)
+               tileHeight = 100 - percentageHill < ratio ? Random.Range(1, tileHeight+1) : 0;// random height hill
+               //int tresholdTreasureTile = widht * (depth / 2);
+                for (int w = 0; w <= tileHeight; w++) // height y
                 {
                     GameObject tile = new GameObject();
-                    tile.name = string.Format("Tile[{0}][{1}][{2}]", i, w, j);
+                    tile.name = string.Format(NAME_PATTERN_TILE, i, w, j);
                     tile.transform.SetParent(row.transform);
                     tile.transform.localPosition = new Vector3(j, w, 0);
                     tile = Instantiate(tilePrefabs, tile.transform);
                     
-                   TileController tileProfile = tile.GetComponent<TileController>();
-                   tileProfile.Heightlvl = w;
-                   tileProfile.IsWalkAble = w < tileHeight? false:true;
-                   tileProfile.IsReachAble = tileProfile.IsWalkAble ? tileProfile.IsWalkAble : false;
-                   if (tileProfile.IsReachAble)
-                    {
-                        if(Random.Range(0,10) % 9 == 0 && treasureCount > 0 && tresholdTreasureTile == (widht*(depth/2)))
-                        {
-                            tileProfile.IsContainItem = true;
-                            treasureCount--;
-                            tresholdTreasureTile = 0;
-                        }
-                        else
-                        {
-                            tileProfile.IsContainItem = false;
-                            tresholdTreasureTile++;
-                        }
-                    }
-                   
+                    TileController tileProfile = tile.GetComponent<TileController>();
+                    tileProfile.TileId = w<tileHeight ?  "Not Reachable" : string.Format(ID_PATTERN_TILE, i, j);
+                    tileProfile.Heightlvl = w;
+                    tileProfile.IsWalkAble = w == tileHeight;
+                    tileProfile.IsCursorSelect = false;
+
+                    if(!tileProfile.TileId.Contains(NOT_REACHABLE)) tileMap.Add(tileProfile.TileId, tile);
+             
                 }
 
             }
