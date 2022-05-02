1fbac8cb74287d2941212dc2333a86983b7997af
aongtosan <aongtosan@gmail.com>
2022-05-02 12:28:33 +0700

    update enemy handler

--- Assets/Resources/Scripts/Handler/EnemyUnitHandler.cs
+++ Assets/Resources/Scripts/Handler/EnemyUnitHandler.cs
@@ -7,14 +7,6 @@ public class EnemyUnitHandler : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] int enemyUnitCount;
     List<EnemyController> enemyList; 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
+    
 }
